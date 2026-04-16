01a4c6c41bffad3052903200ce5478c8d4f04696
KarlTilsner <karltilsner@icloud.com>
2026-04-16 18:16:37 +0930

    Refactor main_processing.py to include match processing comments and add all_amiibo.py script for collecting unique amiibo IDs

---
+++ Data_Processing/all_amiibo.py
@@ -0,0 +1,1 @@
+# This script will collect all unique amiibo IDs and store each of their match history 
--- Data_Processing/main_processing.py
+++ Data_Processing/main_processing.py
@@ -1,8 +1,9 @@
-# Get filenames of all raw match jsons
-# Sort those by their date
-# Start with the earliest date in the list, check the state file for the last date that was processed
-# If the date of the match is later than the last processed date, begin processing the list of matches until all are done
-# Update the state file with the date of the last processed match
+import Data_Processing.all_trainers as all_trainers
 
 def main():
+    # Get filenames of all raw match jsons
+    # Sort those by their date
+    # Start with the earliest date in the list, check the state file for the last date that was processed
+    # If the date of the match is later than the last processed date, begin processing the list of matches until all are done
+    # Update the state file with the date of the last processed match
     pass
