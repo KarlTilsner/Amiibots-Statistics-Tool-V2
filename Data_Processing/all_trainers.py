1281b938ab413d3004f2665d6868f208d59d1fa2
KarlTilsner <karltilsner@icloud.com>
2026-04-16 22:23:01 +0930

    Refactor main_processing.py to improve file handling and processing logic

--- Data_Processing/all_trainers.py
+++ Data_Processing/all_trainers.py
@@ -1,2 +1,11 @@
 # This script will collect all unique trainer IDs and store the IDs of all amiibo associated with each trainer ID
-# 
+
+def main(match):
+    # Get the IDs of the p1 and p2 amiibo
+    # Attempt to append the IDs to the respective trainer ID json
+    # If the trainer ID json doesn't exist, create it and add the amiibo ID to it
+    # If the trainer ID json does exist, check if the amiibo ID is already in
+    # If the amiibo ID is already in, update the name of the amiibo if it is different
+    print(f"Processing match: {len(match['data'])}")
+
+    return
--- Data_Processing/main_processing.py
+++ Data_Processing/main_processing.py
@@ -1,9 +1,59 @@
-import Data_Processing.all_trainers as all_trainers
+import all_trainers as all_trainers
+import all_amiibo as all_amiibo
+import json
+from urllib.parse import quote
+from datetime import datetime
+import os
+
+
+
+# Get filenames of all raw match jsons
+folder = "Data_Collection/Raw_Matches"
+files = [f for f in os.listdir(folder) if f.endswith(".json")]
+
+
+
+def run_functions(f):
+    print(f"Opening file: {f}")
+    with open(os.path.join(folder, f), "r") as file:
+        data = json.load(file)
+
+        # Call the functions to process the matches
+        all_trainers.main(data)
+        # all_amiibo.main(data)
+
+
 
 def main():
-    # Get filenames of all raw match jsons
-    # Sort those by their date
+    # Sort those by their date/filename
+    files.sort()
+
+    last_processed_file = None
+
+    # Check for a state file and load the date of the last processed match, if it exists
+    if os.path.exists('Data_Processing/state.json'):
+        with open('Data_Processing/state.json', "r") as f:
+            state = json.load(f)
+            last_processed_file = state.get("last_processed_file")
+
     # Start with the earliest date in the list, check the state file for the last date that was processed
-    # If the date of the match is later than the last processed date, begin processing the list of matches until all are done
+    for filename in files:
+        # If we have a last processed file, skip older/equal files
+        if last_processed_file and filename <= last_processed_file:
+            continue
+        
+        print(f"Processing: {filename}")
+        run_functions(filename)
+
+        # Update last processed file
+        last_processed_file = filename
+
     # Update the state file with the date of the last processed match
-    pass
+    with open('Data_Processing/state.json', "w") as f:
+        newstate = {
+            "last_processed_file": last_processed_file,
+            "last_processed_date": datetime.now().isoformat()
+        }
+        json.dump(newstate, f , indent=2)
+
+main()
