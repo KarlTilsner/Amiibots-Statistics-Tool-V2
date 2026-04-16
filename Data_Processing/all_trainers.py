753ecbcb4f6c26f656b4d9f6bc6a433dab2464a7
KarlTilsner <karltilsner@icloud.com>
2026-04-16 18:16:05 +0930

    Add initial scripts for collecting unique amiibo and trainer IDs

---
+++ Data_Processing/all_trainers.py
@@ -0,0 +1,2 @@
+# This script will collect all unique trainer IDs and store the IDs of all amiibo associated with each trainer ID
+# 
---
+++ Data_Processing/main_processing.py
@@ -0,0 +1,8 @@
+# Get filenames of all raw match jsons
+# Sort those by their date
+# Start with the earliest date in the list, check the state file for the last date that was processed
+# If the date of the match is later than the last processed date, begin processing the list of matches until all are done
+# Update the state file with the date of the last processed match
+
+def main():
+    pass
