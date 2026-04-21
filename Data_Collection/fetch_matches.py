9001fd2859ab8fa5c366b7570fc4f16a89b71ac0
KarlTilsner <karltilsner@icloud.com>
2026-04-21 23:11:15 +0930

    Reduce the number of pages fetched from 50 to 10 in main function

--- Data_Collection/fetch_matches.py
+++ Data_Collection/fetch_matches.py
@@ -144,7 +144,7 @@ def main(ruleset):
     ruleset_id = ruleset
     data_to_store = []
 
-    for _ in range(50):  # Loop to fetch multiple pages of matches
+    for _ in range(10):  # Loop to fetch multiple pages of matches
         print(f"Fetching {num_matches} matches: {_ + 1}/10")
         start_fetching()
 
